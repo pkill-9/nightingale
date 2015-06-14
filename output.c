f60028cdfdb5cc4d2ec91fd0a61bdd6ffb66ccae
Matthew Signorini <matthew.signorini@gmail.com>
2015-06-14 19:46:36 +1000

    Added a function to print integers in base 10 form, ie human readable numbers.
    It uses a clever recursive algorithm to avoid clumsy buffers.

--- output.c
+++ output.c
@@ -12,6 +12,36 @@ PRIVATE void print_char (char c);
 /**********************************************************/
 
 /**
+ *  Function to print an integer in human readable form (base 10).
+ *
+ *  This uses a recursive algorithm to ensure that we print the digits in
+ *  the correct order (most significant first) without needing some kind
+ *  of a buffer. For each recursive call, the value is divided by 10, and
+ *  then the digits are printed as we come back out from the stack of
+ *  calls. Since the bottom of the stack will have the most significant
+ *  digit, this means that digits will be printed in order of most
+ *  significant to least.
+ */
+    PUBLIC void
+print_integer (value)
+    int value;                  // integer to be printed.
+{
+    // if the value is negative, we will print a - sign first.
+    if (value < 0)
+    {
+        print_char ('-');
+        value = ~value + 1; // two's complement hack for value *= -1.
+    }
+
+    if (value / 10 != 0)
+        print_integer (value / 10);
+
+    print_char ('0' + value % 10);
+}
+
+/**********************************************************/
+
+/**
  *  Function for printing a 32 bit integer in hexadecimal form. This 
  *  function will also print the leading 0x prefix.
  */
