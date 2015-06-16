6c27e74ba3bbeb55c67be6913db92d48cf484231
Matthew Signorini <matthew.signorini@gmail.com>
2015-06-16 20:26:42 +1000

    Added a definition for the memcopy function, which seems to be the only thing left to be completed in order for nightingale to build.

---
+++ memutils.h
@@ -0,0 +1,15 @@
+/**
+ *  Functions for copying data.
+ */
+
+#ifndef _MEMUTILS_H
+#define _MEMUTILS_H
+
+#include "stdint.h"
+
+void memcopy (void *source, void *dest, size_t count);
+
+
+#endif /** _MEMUTILS_H */
+
+/** vim: set ts=4 sw=4 et : */
