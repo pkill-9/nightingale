7219ac91c311e9f25d1f947d71e9e0de0acd6bdd
Matthew Signorini <matthew.signorini@gmail.com>
2015-06-16 19:59:39 +1000

    Added functions to read and write from IO ports.

---
+++ io.h
@@ -0,0 +1,27 @@
+/**
+ *  Functions for reading and writing on IO ports.
+ *
+ *  Intel CPUs have IO ports for communicating with many hardware devices,
+ *  and special assembly instructions to read and write on IO ports. These
+ *  functions are wrappers to these assembly instructions, making it
+ *  simpler to communicate with hardware devices from C code.
+ *
+ *  These functions are implemented in assembly language.
+ */
+
+#ifndef _IO_H
+#define _IO_H
+
+#include "stdint.h"
+
+/**********************************************************/
+
+void outb (uint16_t port, uint8_t value);
+uint8_t inb (uint16_t port);
+
+/**********************************************************/
+
+
+#endif /** _IO_H */
+
+/** vim: set ts=4 sw=4 et : */
---
+++ io.s
@@ -0,0 +1,58 @@
+/**
+ *  Wrapper functions so that C functions can make use of the CPU
+ *  instructions for reading and writing from ports.
+ */
+
+.section .text
+
+/**********************************************************/
+
+/**
+ *  void outb (uint16_t port, uint8_t value)
+ *
+ *  Writes the given value on the given port.
+ */
+    .globl _outb
+_outb:
+    push    %ebp
+    mov     %esp, %ebp
+    push    %edx
+
+# for the outb instruction, the port goes in register dx and the value
+# in register al
+    mov     8(%ebp), %dx
+    mov     10(%ebp), %al
+    outb    %al, %dx
+
+    pop     %edx
+    pop     %ebp
+    ret
+
+/**********************************************************/
+
+/**
+ *  uint8_t inb (uint16_t port)
+ *
+ *  Reads a byte from the specified IO port and returns it.
+ */
+    .globl _inb
+_inb:
+    push    %ebp
+    mov     %esp, %ebp
+    push    %edx
+
+# clear eax register of any existing data.
+    xor     %eax, %eax
+
+# same operands as for outb; port in dx register, value will be read into
+# al.
+    mov     8(%ebp), %dx
+    inb     %dx, %al
+
+    pop     %edx
+    pop     %ebp
+    ret
+
+/**********************************************************/
+
+/** vim: set ts=4 sw=4 et : */
