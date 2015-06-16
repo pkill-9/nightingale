522c84dd0b54e9a717c2e1e7221c27e059dc3776
Matthew Signorini <matthew.signorini@gmail.com>
2015-06-16 20:00:12 +1000

    Implemented the main function. This is really just a hello world demo.

---
+++ main.c
@@ -0,0 +1,18 @@
+/**
+ *  Main function for nightingale.
+ */
+
+#include "output.h"
+#include "utils.h"
+
+/**********************************************************/
+
+    PUBLIC void
+main (void)
+{
+    print_string ("It Works.\n");
+}
+
+/**********************************************************/
+
+/** vim: set ts=4 sw=4 et : */
