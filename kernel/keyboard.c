2a98a6d65e82958c6ae8b55020ab398125d41beb
Matthew Signorini <matthew.signorini@gmail.com>
2015-06-21 21:39:16 +1000

    Implemented functions to translate a scancode into a keypress or release, and call the corresponding handler function.
    Also added functions to initialise the mapping from scancodes to keycodes
    using the scancode as an index, and a function to set all keyhandlers to
    NULL, to indicate that they aren't in use.

--- kernel/keyboard.c
+++ kernel/keyboard.c
@@ -14,6 +14,7 @@
 #include "scancodes.h"
 #include "utils.h"
 
+
 /**********************************************************/
 
 /**
@@ -30,7 +31,15 @@ typedef struct
 }
 keyhandler_t;
 
+
 /**********************************************************/
+
+PRIVATE void initialise_keyboard (void);
+PRIVATE void init_keymap (void);
+PRIVATE void clear_keyhandlers (void);
+
+PRIVATE void keypressed (const char *scancode, const keycode_t *map);
+PRIVATE void keyreleased (const char *scancode, const keycode_t *map);
 
 PRIVATE void alphabet_key_pressed (keycode_t key);
 PRIVATE void number_key_pressed (keycode_t key);
@@ -40,11 +49,122 @@ PRIVATE void lock_key_pressed (keycode_t key);
 PRIVATE void modifier_pressed (keycode_t key);
 PRIVATE void modifier_released (keycode_t key);
 
+
 /**********************************************************/
 
 PRIVATE bool shift_pressed, control_pressed, alt_pressed;
 PRIVATE bool caps_lock, num_lock, scroll_lock;
 PRIVATE keyhandler_t key_event_handlers [NUMKEYCODES];
+
+PRIVATE keycode_t scancode_map [NUM_SCANCODES] = {0};
+PRIVATE keycode_t alt_scancode_map [NUM_SCANCODES] = {0};
+
+
+/**********************************************************/
+
+/**
+ *  Take a scancode and call the corresponding key handler function.
+ *
+ *  If the scancode corresponds to a key release event (0xF0 prefix) then
+ *  this function will call the keyreleased function to handle it.
+ */
+    PRIVATE void
+keypressed (scancode, map)
+    const char *scancode;       // scancode from the keyboard.
+    const keycode_t *map;       // scancode/keycode mapping to use.
+{
+    keycode_t keycode;
+
+    // check the first byte of the scancode for prefixes that indicate to
+    // use the extended keymap, or that it is a key release.
+    switch (*scancode)
+    {
+    case 0xE0:
+        keypressed (scancode + 1, alt_scancode_map);
+        break;
+
+    case 0xF0:
+        keyreleased (scancode + 1, map);
+        break;
+
+    default:
+        keycode = map [(uint8_t) *scancode];
+        key_event_handlers [keycode].onpress (keycode);
+    }
+}
+
+/**********************************************************/
+
+/**
+ *  Call the key release function for the given key. Key release scancodes
+ *  have the same value as when the key is pressed, but with an 0xF0
+ *  prefix byte before the scancode, which will be picked up by the 
+ *  keypressed function.
+ */
+    PRIVATE void
+keyreleased (scancode, map)
+    const char *scancode;       // scancode without the prefix.
+    const keycode_t *map;       // scancode-keycode mapping to use.
+{
+    keycode_t keycode;
+
+    // XXX: extended scancode map prefix comes before the key release
+    // prefix, so we should never see an 0xE0 prefix here, right?
+    switch (*scancode)
+    {
+    case 0xE0:
+        keyreleased (scancode + 1, alt_scancode_map);
+        break;
+
+    default:
+        keycode = map [(uint8_t) *scancode];
+        key_event_handlers [keycode].onrelease (keycode);
+    }
+}
+
+/**********************************************************/
+
+/**
+ *  Create a mapping from scancode to keycode, with the scancode as an
+ *  index to allow O(1) lookup times. Note that scancodes.h defines the
+ *  association from scancode to keycode, but to use that directly would
+ *  require us to do a search over the association array, which is not
+ *  ideal.
+ */
+    PRIVATE void
+init_keymap (void)
+{
+    // there are two separate mappings, one for "regular" keys, and 
+    // another for keys whose scancodes have the 0xE0 prefix.
+    for (int i = 0; i < NUM_REGULAR_KEYS; i ++)
+    {
+        scancode_map [regular_keys [i].scancode] = 
+            regular_keys [i].keycode;
+    }
+
+    for (int i = 0; i < NUM_EXTENDED_KEYS; i ++)
+    {
+        alt_scancode_map [extended_keys [i].scancode] =
+            extended_keys [i].keycode;
+    }
+}
+
+/**********************************************************/
+
+/**
+ *  Step through the array of key handlers and set the press and release
+ *  function pointers to NULL, just in case there is some garbage in the
+ *  memory.
+ */
+    PRIVATE void
+clear_keyhandlers (void)
+{
+    for (int i = 0; i < NUMKEYCODES; i ++)
+    {
+        key_event_handlers [i].onpress = NULL;
+        key_event_handlers [i].onrelease = NULL;
+    }
+}
 
 /**********************************************************/
 
